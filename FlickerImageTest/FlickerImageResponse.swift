ea786cb26c0fbfc2b5f950a9b52699bcff1cbe0b
JoeyNash <jrnash621@gmail.com>
2024-06-06 09:25:48 -0400

    Codable

---
+++ FlickerImageTest/FlickerImageResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FlickerImageResponse.swift
+//  FlickerImageTest
+//
+//  Created by Joseph Nash on 6/6/24.
+//
+
+import Foundation
+
+struct FlickrResponse: Codable {
+  let items: [FlickrItem]
+}
+
+struct FlickrItem: Codable {
+  let title: String
+  let media: FlickrMedia
+  let dateTaken: Date
+  let description: String
+  let author: String
+  let tags: String
+  private enum CodingKeys: String, CodingKey {
+    case title, media, description, author, tags
+    case dateTaken = "date_taken"
+  }
+}
+
+struct FlickrMedia: Codable {
+  let imageURL: String
+  private enum CodingKeys: String, CodingKey {
+    case imageURL = "m"
+  }
+}
